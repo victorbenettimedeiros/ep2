0a25559d74c6aa129383786c60ef516cd18f5ab1
Francisco Chebib <franciscovc@al.insper.edu.br>
2026-05-01 15:20:58 -0300

    imprime cartela francisco

--- funcoes.py
+++ funcoes.py
@@ -148,3 +148,21 @@ def faz_jogada (lista, categoria, informacoes):
     pontos = calcula_pontos_regra_avancada(lista)
     informacoes['regra_avancada'][categoria] = pontos[categoria]
   return informacoes
+
+#exercicio 13
+def imprime_cartela(cartela):
+    print("Cartela de Pontos:")
+    print("-"*25)    
+    for i in range(1, 7):
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_simples'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_simples'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    for i in cartela['regra_avancada'].keys():
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_avancada'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_avancada'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    print("-"*25)
