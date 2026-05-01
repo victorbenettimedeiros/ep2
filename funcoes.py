2649a7ce477ed1df66830b29110448611d7a1800
Victor <victorbm5@al.insper.edu.br>
2026-05-01 12:03:38 -0300

    Guardar dados victor

--- funcoes.py
+++ funcoes.py
@@ -7,4 +7,14 @@ def rolar_dados(n):
     valor = random.randint(1, 6)
     dados.append(valor)
   return dados
+
+
+def guardar_dado(rolados, estoque, guardar):
+  lista=[]
+  rol=[]
+  i=guardar
+  estoque.append(rolados[i])
+  for d in range(len(rolados)):
+    if d!=i:
+      rol.append(rolados[d])
   
