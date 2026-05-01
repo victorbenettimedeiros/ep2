659069636cf378ea458e3b932011a277ac45425a
Francisco Chebib <franciscovc@al.insper.edu.br>
2026-05-01 15:11:07 -0300

    pontos regra avancada francisco

--- funcoes.py
+++ funcoes.py
@@ -127,3 +127,14 @@ def calcula_pontos_quina (dados):
     if qtd >= 5:
       return 50
   return 0
+
+#exercicio 11
+def calcula_pontos_regra_avancada (lista):
+  dic = {}
+  dic['cinco_iguais'] = calcula_pontos_quina(lista)
+  dic['full_house'] = calcula_pontos_full_house(lista)
+  dic['quadra'] = calcula_pontos_quadra(lista)
+  dic['sem_combinacao'] = calcula_pontos_soma(lista)
+  dic['sequencia_alta'] = calcula_pontos_sequencia_alta(lista)
+  dic['sequencia_baixa'] = calcula_pontos_sequencia_baixa(lista)
+  return dic
